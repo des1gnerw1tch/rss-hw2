51d59690eb132b676a68d3f5956df6de363d7525
des1gnerw1tch <zachcanon@gmail.com>
2025-02-10 21:06:08 -0500

    Finished C6

--- cspace/C6.py
+++ cspace/C6.py
@@ -13,9 +13,9 @@ import typing
 """
 CS4610/CS5335 - Spring 2025 - Homework 2
 
-Name:
-Email:
-With Whom you discussed the questions with:
+Name: Zachary Walker-Liang
+Email: walker-liang.z@northeastern.edu
+With Whom you discussed the questions with: Nobody yet
 """
 
 def C6_func(robot: typing.Dict[str, typing.List[float]], 
@@ -46,7 +46,59 @@ def C6_func(robot: typing.Dict[str, typing.List[float]],
     ### Insert your code below: ###
     num_collisions: int = 0
     fig, ax = plt.subplots()
-    ax.set_title("Swept Volume Collisions")
-    # DO NOT CALL plt.show() inside this function. It is called in hw2_cspace.py
 
-    return num_collisions, fig
+    plt.xlim([0, 12])
+    plt.ylim([0, 12])
+    plt.axis('square')
+
+    ax.set_title("Swept Volume Collisions")
+
+    for i in range(len(q_path)):
+        if (i - 1 < 0):
+            continue
+        arm1Verts, arm2Verts, pivot1, pivot2 = q2poly(robot, q_path[i]) # Arm 1 and 2
+        prevArm1Verts, prevArm2Verts, prevPivot1, prevPivot2 = q2poly(robot, q_path[i - 1])
+        foundCollision, convexHullArm1, convexHullArm2 = IsConvexHullIntersectingWithObstacles(arm1Verts, arm2Verts, prevArm1Verts, prevArm2Verts, obstacles)
+        if (foundCollision):
+            PlotPolygonsToAx(obstacles, convexHullArm1.tolist(), convexHullArm2.tolist(), ax)
+            num_collisions+=1
+    # DO NOT CALL plt.show() inside this function. It is called in hw2_cspace.py
+    return num_collisions, fig    
+
+
+def IsConvexHullIntersectingWithObstacles(arm1Verts: np.array,
+                                        arm2Verts: np.array, 
+                                        prevArm1Verts: np.array,
+                                        prevArm2Verts: np.array,
+                                        obstacles: typing.List[Polygon]) -> tuple[bool, np.array, np.array]:
+                                        
+    obstaclePolygons = []
+    for obstacle in obstacles:
+        obstaclePolygons.append(Polygon_shapely(obstacle))
+    
+    unionArm1 = shapely.union(Polygon_shapely(arm1Verts), Polygon_shapely(prevArm1Verts))
+    unionArm2 = shapely.union(Polygon_shapely(arm2Verts), Polygon_shapely(prevArm2Verts))
+    convexHull1 = shapely.convex_hull(unionArm1)
+    convexHull2 = shapely.convex_hull(unionArm2)
+    for obstacle in obstaclePolygons:
+        if obstacle.intersects(convexHull1) or obstacle.intersects(convexHull2):
+            return True, np.array(list(convexHull1.exterior.coords)), np.array(list(convexHull2.exterior.coords))
+    
+    return False, np.array(list(convexHull1.exterior.coords)), np.array(list(convexHull2.exterior.coords))
+    
+    
+
+def PlotPolygonsToAx(obstacles: typing.List[Polygon],
+                     arm1Hull: typing.List[typing.List[float]],
+                     arm2Hull: typing.List[typing.List[float]], 
+                     ax) -> None:
+    for i in range(len(obstacles)):
+        coord = obstacles[i]
+        p = Polygon(coord, facecolor = 'k')
+        ax.add_patch(p)
+    
+    arm1Hull_p = Polygon(arm1Hull, facecolor = 'r', alpha=0.3)
+    arm1Hull_p = ax.add_patch(arm1Hull_p)
+
+    arm2Hull_p = Polygon(arm2Hull, facecolor = 'b', alpha=0.3)
+    arm2Hull_p = ax.add_patch(arm2Hull_p)
