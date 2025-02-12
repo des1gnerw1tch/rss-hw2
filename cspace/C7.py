1e33b6b4bfb37462e1f78407c28b8be4bde3a757
des1gnerw1tch <zachcanon@gmail.com>
2025-02-12 14:29:56 -0500

    Finished C7

--- cspace/C7.py
+++ cspace/C7.py
@@ -7,9 +7,9 @@ from helper_functions import *
 """
 CS4610/CS5335 - Spring 2025 - Homework 2
 
-Name:
-Email:
-With Whom you discussed the questions with:
+Name: Zachary Walker-Liang
+Email: walker-liang.z@northeastern.edu
+With Whom you discussed the questions with: Oliver Hugh
 """
 
 def C7_func(cspace: np.array) -> np.array:
@@ -27,6 +27,16 @@ def C7_func(cspace: np.array) -> np.array:
     """
 
     ### Insert your code below: ###
+
     padded_cspace = cspace.copy()
-    
+    for row in range(cspace.shape[0]):
+        for col in range(cspace.shape[1]):
+            if (padded_cspace[row][col] == 1):
+                continue
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if row + i < 0 or row + i >= cspace.shape[0] or col + j < 0 or col + j >= cspace.shape[1]:
+                        continue
+                    if (cspace[row + i][col + j] == 1): # If neighbor is a wall
+                        padded_cspace[row][col] = 1
     return padded_cspace
